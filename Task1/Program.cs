038fe90f11fb86cc0a0de87f4de333ec069b5b52
Artem.don <dontsovki@gmail.com>
2023-02-02 21:12:18 +0300

    Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.

---
+++ Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int [,] matr) // Заполнение массива рандомными данными
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void PrintArray(int[,] matr) // Распечатывание массива
+{
+    for (int i = 0; i  < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+void SortingElements(int[,] array)  // Функция сортировки элементов в строке двумерного массива, по убыванию
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void PrintArraySorting(int[,] array) // Распечатывание массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] massiv = new int [4, 4];
+FillArray(massiv);
+// PrintArray(massiv);
+PrintArray(massiv);
+
+Console.WriteLine();
+Console.WriteLine();
+
+SortingElements(massiv);
+PrintArraySorting(massiv);
